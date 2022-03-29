b73f0a415596f5444148c902cffa64ac117c744b
Henrique Florencio <henrique.florencio307@gmail.com>
2022-03-29 17:42:04 -0300

    mudando nomes...

--- src/Orientacao a Objetos/TipoosDeConta.kt
+++ src/Orientacao a Objetos/TipoosDeConta.kt
@@ -1,38 +1,41 @@
 package `Orientacao a Objetos`
+
 class ContaPadrao : Conta {
     constructor(
         nome: String,
         senha: String,
         gold: Int = 0
-    ):super(
+    ) : super(
         nome = nome,
         senha = senha,
         gold = gold
     );
 
     override fun adicionaGold(valor: Int) {
-        class ContaVIP : Conta {
-            constructor(
-                nome: String,
-                senha: String,
-                gold: Int = 0
-            ) : super(
-                nome = nome,
-                senha = senha,
-                gold = gold,
-            ) {
-            }
-
-            override fun adicionaGold(valor: Int) {
-                var valorSomado = valor + 10
-
-                if (this.conectado) {
-                    this.gold += valorSomado
-                }
-            }
-        }
         if (this.conectado) {
             this.gold += valor
         }
     }
 }
+
+class ContaVIP : Conta {
+    constructor(
+        nome: String,
+        senha: String,
+        gold: Int = 0
+    ) : super(
+        nome = nome,
+        senha = senha,
+        gold = gold,
+    ) {
+    }
+
+    override fun adicionaGold(valor: Int) {
+        var valorSomado = valor + 10
+
+        if (this.conectado) {
+            this.gold += valorSomado
+        }
+    }
+}
+
